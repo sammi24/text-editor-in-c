42d71a90ac7467fc89549da70cc650119e195baa
SavviSam <swghazzawi@gmail.com>
2024-02-16 16:18:23 -0700

    edit temrinal attribs

--- kilo.c
+++ kilo.c
@@ -1,3 +1,43 @@
-int main() {
+#include <ctype.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios; // original terminal attributes
+
+void disableRawMode()
+{
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); // when registered, sets terminal back to original attributes
+}
+
+void enableRawMode()
+{
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+
+  struct termios raw = orig_termios; // terminal attribute being updated
+  raw.c_iflag &= ~(IXON | ICRNL);
+  raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main()
+{
+  enableRawMode();
+
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q')
+  {
+    if (iscntrl(c))
+    {
+      printf("%d\n", c);
+    }
+    else
+    {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
   return 0;
 }
